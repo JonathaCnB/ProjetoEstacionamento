c85eefaf117f40e62505194d9edfe04ddac7307f
JonathaCnB <jotacarlosftb@gmail.com>
2021-01-02 21:58:35 -0400

    Adicionado model paramentos para configurar valores, adicionado campo proprietario a class Veiculos

--- core/models.py
+++ core/models.py
@@ -12,14 +12,28 @@ class Pessoa(models.Model):
     endereco = models.CharField(max_length=200)
     telefone = models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.nome + '-' + self.telefone
+
 
 class Marca(models.Model):
     nome = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.nome
+
 
 class Veiculo(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.SET(get_sentinel_user), )
+    proprietario = models.ForeignKey(Pessoa, on_delete=models.SET(get_sentinel_user), )
     placa = models.CharField(max_length=7)
     cor = models.CharField(max_length=15)
     observacoes = models.TextField()
 
+    def __str__(self):
+        return self.placa + '-' + self.marca.nome
+
+
+class Parametros(models.Model):
+    valor_hora = models.DecimalField(max_digits=5, decimal_places=2)
+    valor_mes = models.DecimalField(max_digits=6, decimal_places=2)
