49c821a7723d2dda3c8a2d4e31708e8f9e0b0cfe
JonathaCnB <jotacarlosftb@gmail.com>
2021-01-03 14:46:03 -0400

    Comeco da configuracao do sistemas de template, feito duas formas de fazer templates a 1 pela pasta templates dentro da app e nao necessita de configurar o arq settings, a 2 com uma pasta templates na raiz do projeto porem essa pasta precisa ser configurada no arquivo settings.

--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-# from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render
+# from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Ola Mundo')
+    context = {'mensagem': 'Ola mundo'}
+    # return render(request, 'core/index.html', context) essa opção não requer configuração do arquivo setting
+    # Apenas criar na pasta da app uma pasta /template/nomadaApp/
+    return render(request, 'base.html', context) # Essa opção esta puxando essa aquivo da pasta raiz chamada template
+    # Porem precisa ser configurado no settins
+
