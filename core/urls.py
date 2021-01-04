ab5fe1d98d59c02c9ba44c7645472c991ebcb697
JonathaCnB <jotacarlosftb@gmail.com>
2021-01-03 21:17:31 -0400

    concluido a incremetacao da lista de movimentacao rotativa e lista de mensalista, feito todos os processos urls, views e criado uma pagina html para cada processo

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,17 @@
 from django.urls import path
-from .views import home, lista_pessoas, lista_veiculos
+from .views import (
+    home,
+    lista_pessoas,
+    lista_veiculos,
+    lista_movrotativos,
+    lista_mensalista,
+)
 
 
 urlpatterns = [
     path('', home, name='core_home'),
     path('pessoas/', lista_pessoas, name='core_lista_pessoas'),
     path('veiculos/', lista_veiculos, name='core_lista_veiculos'),
+    path('mov-rotativo/', lista_movrotativos, name='core_lista_movrotativo'),
+    path('mensalista/', lista_mensalista, name='core_lista_mensalista'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
-from .models import Pessoa, Veiculo
+from .models import Pessoa, Veiculo, MovRotativo, MovMensalista
 
 
 def home(request):
@@ -19,3 +19,14 @@ def lista_pessoas(request):
 def lista_veiculos(request):
     veiculos = Veiculo.objects.all()
     return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+
+def lista_movrotativos(request):
+    mov_rotativos = MovRotativo.objects.all()
+    return render(request, 'core/lista_movrotativos.html',
+                  {'mov_rotativos': mov_rotativos})
+
+
+def lista_mensalista(request):
+    mensalista = MovMensalista.objects.all()
+    return render(request, 'core/lista_mensalistas.html', {'mensalista': mensalista})
